12c83f4fa19d268c10d675499d713bbaf5eaf9bb
jahir2004 <jahirgyanbaloche@Gmail.com>
2026-03-15 20:29:30 +0100

    Wijzigingen in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AllergeenController;
+use App\Http\Controllers\GeleverdController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +15,10 @@ Route::get('/dashboard', function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/allergenen', [AllergeenController::class, 'index'])->name('allergenen.index');
     Route::get('/allergenen/leverancier/{productId}', [AllergeenController::class, 'leverancier'])->name('allergenen.leverancier');
+});
+Route::middleware(['auth'])->group(function () {
+    Route::get('/geleverd', [GeleverdController::class, 'index'])->name('geleverd.index');
+    Route::get('/geleverd/{product}/specificatie', [GeleverdController::class, 'specificatie'])->name('geleverd.specificatie');
 });
 
 require __DIR__.'/auth.php';
